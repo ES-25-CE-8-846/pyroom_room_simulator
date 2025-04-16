40e67711d68163cb1be7ab50e5d6e7c23c95a5f8
Andreas Højrup <andreas.hovaldt@gmail.com>
2025-04-16 11:19:37 +0200

    Improvements: - Added way to define random room bounds - Added way to define material property randomness bounds - Added a check for using or not using ray tracing - Fixed room parameters being reset unintentionally
    Co-authored-by: TheMipmap <morten-ks@hotmail.com>

--- tools/room_generator.py
+++ tools/room_generator.py
@@ -5,13 +5,19 @@ import logging
 logger = logging.getLogger(__name__)
 
 class RoomGenerator:
-    def __init__(self, corners=None, shape: str = "shoebox", material_properties=None, fs=48000, ray_tracing_params=None, extrude_height=None):
+    def __init__(self, 
+                 corners=None, 
+                 shape: str = "shoebox", 
+                 material_properties_bounds={"energy_absorption": None, "scattering": None}, 
+                 fs=48000, 
+                 ray_tracing_params=None, 
+                 extrude_height=None):
         """
         Initialize the RoomGenerator.
 
         Parameters:
             corners (np.array): Array of room corners [x, y].
-            material_properties (dict): Dictionary with 'energy_absorption' and 'scattering'.
+            material_properties_bounds (dict): Dictionary with 'energy_absorption' and 'scattering' bounds.
             fs (int): Sampling frequency.
             ray_tracing_params (dict): Dictionary with 'receiver_radius', 'n_rays', and 'energy_thres'.
             extrude_height (float): Height to extrude the room.
@@ -19,17 +25,19 @@ class RoomGenerator:
         self.corners = corners
         assert shape in ["shoebox", "t_room", "l_room"], f"Invalid room shape: {shape}. Must be one of ['shoebox', 't_room', 'l_room']."
         self.shape = shape
-        self.material_properties = material_properties
+        self.material_properties_bounds = material_properties_bounds
         self.fs = fs
+        self.use_ray_tracing = True if ray_tracing_params is not None else False
         self.ray_tracing_params = ray_tracing_params
         self.extrude_height = extrude_height
 
     # TODO: Needs a v2
-    def _compose_random_shoebox(self, np_random_generator: np.random.Generator, min_width = 3.0, max_width = 10.0, min_length = 3.0, max_length = 10.0, min_extrude = 2.0) -> tuple[np.ndarray, float]:
+    def _compose_random_shoebox(self, np_random_generator: np.random.Generator, min_width = 3.0, max_width = 10.0, min_length = 3.0, max_length = 10.0, min_extrude = 2.0, max_extrude = 5.0, **kwargs) -> tuple[np.ndarray, float]:
         """
         Compose a shoebox room with the specified corners and material properties.
         """
-        logger.debug("Generating random shoebox room")
+        logger.info("Generating random shoebox room")
+        logger.info(f"Bounds: {min_width}, {max_width}, {min_length}, {max_length}, {min_extrude}, {max_extrude}")
         
         # Generate random width and length
         width = np_random_generator.uniform(min_width, max_width)
@@ -37,7 +45,7 @@ class RoomGenerator:
         corners = np.array([[0, 0], [0, width], [length, width], [length, 0]])
         
         if self.extrude_height is None:
-            extrude_height = np_random_generator.uniform(max(min_extrude, min(width, length)), min(width, length)) # Ensure that the room is at least 2m high
+            extrude_height = np_random_generator.uniform(min_extrude, max_extrude)
             return corners, extrude_height
         
         return corners, self.extrude_height
@@ -61,7 +69,7 @@ class RoomGenerator:
         return corners, extrude_height
     
     
-    def generate_room(self, seed=None):
+    def generate_room(self, seed=None, room_bounds=None) -> tuple[pra.Room, dict]:
         """
         Generate the room with the specified properties. If ´corners´ is not set, a room with random width and length is generated.
         """
@@ -73,52 +81,61 @@ class RoomGenerator:
         # If corners is not set, generate random corners
         if self.corners is None:
             if self.shape == "shoebox":
-                self.corners, self.extrude_height = self._compose_random_shoebox(np_random_generator=random_gen)
+                corners, extrude_height = self._compose_random_shoebox(np_random_generator=random_gen, **room_bounds)
             elif self.shape == "t_room":
-                self.corners, self.extrude_height = self._compose_random_troom()
+                corners, extrude_height = self._compose_random_troom()
             elif self.shape == "l_room":
-                self.corners, self.extrude_height = self._compose_random_lroom()
+                corners, extrude_height = self._compose_random_lroom()
         
         
         # If corners is set, use them to generate the room
         else:
             width = np.max(self.corners[1, :]) - np.min(self.corners[1, :])
             length = np.max(self.corners[0, :]) - np.min(self.corners[0, :])
-            self.corners = np.array([[0, 0], [0, width], [length, width], [length, 0]])
-            self.extrude_height = random_gen.uniform(min(2.0, width, length), min(width, length))
+            corners = np.array([[0, 0], [0, width], [length, width], [length, 0]])
+            extrude_height = random_gen.uniform(min(2.0, width, length), min(width, length))
 
-        if self.material_properties is None:
-            self.material_properties = {
+        # If material properties are not set, use default values
+        if self.material_properties_bounds is None:
+            material_properties = {
                 'energy_absorption': random_gen.uniform(0.1, 0.9),
                 'scattering': random_gen.uniform(0.1, 0.9)
             }
-        if self.ray_tracing_params is None:
-            self.ray_tracing_params = {
-                'receiver_radius': 0.1,
-                'n_rays': 1000,
-                'energy_thres': 1e-5
+        else:
+            material_properties = {
+                'energy_absorption': random_gen.uniform(self.material_properties_bounds['energy_absorption'][0], self.material_properties_bounds['energy_absorption'][1]),
+                'scattering': random_gen.uniform(self.material_properties_bounds['scattering'][0], self.material_properties_bounds['scattering'][1])
+            }
+        logger.info(f"Material properties: {material_properties}")
+        
+        # If ray tracing parameters are not set, use default values
+        if self.use_ray_tracing:
+            logger.info("Using ray tracing")
+            ray_tracing_params = {
+                'receiver_radius': self.ray_tracing_params['receiver_radius'],
+                'n_rays': self.ray_tracing_params['n_rays'],
+                'energy_thres': self.ray_tracing_params['energy_thres']
             }
         
 
-        material = pra.Material(energy_absorption=self.material_properties['energy_absorption'],
-                                scattering=self.material_properties['scattering'])
-        room = pra.Room.from_corners(self.corners.T, materials=material, fs=self.fs, ray_tracing=True, air_absorption=True)
-        room.extrude(self.extrude_height)
-        room.set_ray_tracing(receiver_radius=self.ray_tracing_params['receiver_radius'],
-                             n_rays=self.ray_tracing_params['n_rays'],
-                             energy_thres=self.ray_tracing_params['energy_thres'])
+        material = pra.Material(energy_absorption=material_properties['energy_absorption'],
+                                scattering=material_properties['scattering'])
+        room = pra.Room.from_corners(corners.T, materials=material, fs=self.fs, ray_tracing=self.use_ray_tracing, air_absorption=True, max_order=10)
+        room.extrude(extrude_height)
+        
+        if self.use_ray_tracing:
+            logger.info(f"Ray tracing parameters: {ray_tracing_params}")
+            room.set_ray_tracing(receiver_radius=ray_tracing_params['receiver_radius'],
+                                n_rays=ray_tracing_params['n_rays'],
+                                energy_thres=ray_tracing_params['energy_thres'])
+        
         dimensions = {
             'width': room.get_bbox()[0,1],
             'length': room.get_bbox()[1,1],
-            'height': self.extrude_height
+            'height': extrude_height
         }
-        # Reset attributes to None
-        self.corners = None
-        self.material_properties = None
-        self.ray_tracing_params = None
-        self.extrude_height = None
 
-        return room , dimensions
+        return room, dimensions
     
 if __name__ == "__main__":
     room_generator = RoomGenerator()
